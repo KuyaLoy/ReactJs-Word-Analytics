ba32ac9cb74a660bda12e2b1aff77f2df7bcf02b
Robin Tapiru <robintapiru0894@gmail.com>
2024-07-24 17:01:09 +0400

    Reusable Component (Stat Component)

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,22 +1,19 @@
 export default function Stats() {
   return (
     <section className="stats">
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Words</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Characters</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">280</span>
-        <h2 className="second-heading">Instagram</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">2200</span>
-        <h2 className="second-heading">Facebook</h2>
-      </section>
+      <Stat number={0} label="word" />
+      <Stat number={0} label="character" />
+      <Stat number={280} label="instagram" />
+      <Stat number={2200} label="facebook" />
     </section>
   );
+
+  function Stat({ label, number }) {
+    return (
+      <section className="stat">
+        <span className="stat__number">{number}</span>
+        <h2 className="second-heading">{label}</h2>
+      </section>
+    );
+  }
 }
